ad3bb3d2ecfb9ae26756071cf9207a0bd60e26cd
Christopher Hern <christopher.t.hern@gmail.com>
2025-08-31 05:43:37 -0500

    [FIX] even more fixes

--- src/rdma_cm_helpers.h
+++ src/rdma_cm_helpers.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #include <rdma/rdma_cma.h>
 #include "common.h"
@@ -7,5 +6,9 @@
 int cm_create_channel_and_id(rdma_ctx *c);
 int cm_server_listen(rdma_ctx *c, const char *port);
 int cm_wait_event(rdma_ctx *c, enum rdma_cm_event_type want, struct rdma_cm_event **out);
+
+/* NEW: make sure this line exists */
+int cm_wait_connected(rdma_ctx *c, struct rdma_conn_param *out_conn_param);
+
 int cm_server_accept_with_priv(rdma_ctx *c, const void *priv, size_t len);
 int cm_client_connect(rdma_ctx *c, const char *ip, const char *port);
