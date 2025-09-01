222c351193e15f88e5f77f8df95b9741ec266629
Christopher Hern <christopher.t.hern@gmail.com>
2025-09-01 05:03:11 -0500

    [FIX] IMM client

--- src/client_imm.c
+++ src/client_imm.c
@@ -1,11 +1,12 @@
 
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
-#include <arpa/inet.h>
+
 #include "common.h"
-#include "rdma_ctx.h"
-#include "rdma_cm_helpers.h"
 #include "rdma_builders.h"
+#include "rdma_cm_helpers.h"
+#include "rdma_ctx.h"
 #include "rdma_mem.h"
 #include "rdma_ops.h"
 
@@ -19,40 +20,59 @@ int main(int argc, char **argv) {
   const char *ip = argv[1];
   const char *port = argv[2];
 
+  // --- connect & setup (new flow) ---
   rdma_ctx c = {0};
   LOG("Create CM channel + ID + connect");
   cm_create_channel_and_id(&c);
-  cm_client_connect(&c, ip, port);
 
-  struct rdma_cm_event *ev;
-  CHECK(cm_wait_event(&c, RDMA_CM_EVENT_ESTABLISHED, &ev), "ESTABLISHED");
+  // 1) Resolve first (ADDR/ROUTE)
+  CHECK(cm_client_resolve(&c, ip, port), "resolve");
+
+  // 2) Build PD/CQ/QP BEFORE rdma_connect
+  LOG("Build PD/CQ/QP");
+  build_pd_cq_qp(&c, IBV_QPT_RC, 64, 32, 32, 1);
+
+  // 3) rdma_connect with tiny credits (SoftRoCE-friendly)
+  CHECK(cm_client_connect_only(&c, 1, 1), "rdma_connect");
+
+  // 4) Wait for ESTABLISHED and pull private_data safely
+  struct rdma_conn_param connp = {0};
+  CHECK(cm_wait_connected(&c, &connp), "ESTABLISHED");
+
   struct remote_buf_info info = {0};
-  if (ev->param.conn.private_data && ev->param.conn.private_data_len >= sizeof(info))
-      memcpy(&info, ev->param.conn.private_data, sizeof(info));
-  else { fprintf(stderr, "No private_data\n"); return 2; }
-  rdma_ack_cm_event(ev);
+  if (connp.private_data && connp.private_data_len >= sizeof(info)) {
+    memcpy(&info, connp.private_data, sizeof(info));
+  } else {
+    fprintf(stderr, "No private_data\n");
+    return 2;
+  }
   c.remote_addr = ntohll_u64(info.addr);
   c.remote_rkey = ntohl(info.rkey);
-  LOG("Got remote addr=%#lx rkey=0x%x", (unsigned long)c.remote_addr, c.remote_rkey);
+  LOG("Got remote addr=%#lx rkey=0x%x", (unsigned long)c.remote_addr,
+      c.remote_rkey);
 
   LOG("Build PD/CQ/QP");
   build_pd_cq_qp(&c, IBV_QPT_RC, 64, 32, 32, 1);
 
   LOG("Register local tx");
   alloc_and_reg(&c, &c.buf_tx, &c.mr_tx, BUF_SZ, IBV_ACCESS_LOCAL_WRITE);
-  strcpy((char*)c.buf_tx, "client-wrote-with-imm");
+  strcpy((char *)c.buf_tx, "client-wrote-with-imm");
 
   // RDMA_WRITE_WITH_IMM: include imm_data (e.g., payload length or tag)
-  uint32_t imm = htonl((uint32_t)(strlen((char*)c.buf_tx)+1));
+  uint32_t imm = htonl((uint32_t)(strlen((char *)c.buf_tx) + 1));
 
-  struct ibv_sge s = {.addr=(uintptr_t)c.buf_tx, .length=(uint32_t)strlen((char*)c.buf_tx)+1, .lkey=c.mr_tx->lkey};
-  struct ibv_send_wr wr = {
-    .wr_id=1, .sg_list=&s, .num_sge=1,
-    .opcode=IBV_WR_RDMA_WRITE_WITH_IMM,
-    .send_flags=IBV_SEND_SIGNALED,
-    .wr.rdma = {.remote_addr=c.remote_addr, .rkey=c.remote_rkey},
-    .imm_data = imm
-  }, *bad=NULL;
+  struct ibv_sge s = {.addr = (uintptr_t)c.buf_tx,
+                      .length = (uint32_t)strlen((char *)c.buf_tx) + 1,
+                      .lkey = c.mr_tx->lkey};
+  struct ibv_send_wr wr = {.wr_id = 1,
+                           .sg_list = &s,
+                           .num_sge = 1,
+                           .opcode = IBV_WR_RDMA_WRITE_WITH_IMM,
+                           .send_flags = IBV_SEND_SIGNALED,
+                           .wr.rdma = {.remote_addr = c.remote_addr,
+                                       .rkey = c.remote_rkey},
+                           .imm_data = imm},
+                     *bad = NULL;
   dump_sge(&s, "WRITE_WITH_IMM");
   dump_wr_rdma(&wr);
   LOG("Post RDMA_WRITE_WITH_IMM (imm=host %u / net 0x%x)", ntohl(imm), imm);
--- src/rdma_cm_helpers.h
+++ src/rdma_cm_helpers.h
@@ -19,3 +19,4 @@ int cm_client_connect(rdma_ctx *c, const char *ip, const char *port);
 int cm_client_resolve(rdma_ctx *c, const char *ip, const char *port);
 int cm_client_connect_only(rdma_ctx *c, uint8_t initiator_depth,
                            uint8_t responder_resources);
+int cm_wait_connected(rdma_ctx *c, struct rdma_conn_param *out_conn_param);
